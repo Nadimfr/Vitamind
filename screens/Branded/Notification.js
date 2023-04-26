71b851e145c6232983e1f1fcde0af40affad461e
LAPTOP-4C6P74KC\user <201810616@ua.edu.lb>
2023-04-26 18:30:05 +0300

    notification + register screens

---
+++ screens/Branded/Notification.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Header from "../../components/Header";
+
+function Notification() {
+  return (
+    <View style={{ height: "100%", backgroundColor: "white" }}>
+      <Header
+        dots
+        screenName={"Notifications"}
+        onBack={() => navigation.navigate("Home")}
+      />
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#42A45C",
+          marginTop: 10,
+        }}
+      ></View>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "flex-cen",
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <View
+          style={{
+            marginLeft: 15,
+            width: 10,
+            height: 10,
+            borderRadius: 50,
+            backgroundColor: "#42A45C",
+            alignSelf: "center",
+          }}
+        ></View>
+        <Text style={styles.optionstitle}>You completed 3 tasks today!</Text>
+      </View>
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#42A45C",
+          marginTop: 10,
+        }}
+      ></View>
+      <View style={{}}>
+        <Text style={styles.optionstitle}>You completed 3 tasks today!</Text>
+      </View>
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#42A45C",
+          marginTop: 10,
+        }}
+      ></View>
+      <View style={{}}>
+        <Text style={styles.optionstitle}>You completed 3 tasks today!</Text>
+      </View>
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#42A45C",
+          marginTop: 10,
+        }}
+      ></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  optionstitle: {
+    textAlign: "left",
+    fontSize: 22,
+    fontFamily: "Lato",
+    alignSelf: "left",
+    color: "#142F21",
+    marginLeft: 20,
+    marginTop: 10,
+  },
+});
+
+export default Notification;
--- screens/Unbranded/Register.js
+++ screens/Unbranded/Register.js
@@ -1,10 +1,153 @@
-import React from "react";
-import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useContext, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Button from "../../components/Button";
+import TextField from "../../components/TextField";
+import * as Animatable from "react-native-animatable";
 
 function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    fullname: "",
+    confirmpassword: "",
+  });
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <View style={{ height: 200, width: "100%", backgroundColor: "red" }}></View>
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <KeyboardAvoidingView behavior="position">
+        <Text style={[styles.text, { color: "#142F21" }]}>Welcome to</Text>
+        <Text
+          style={[
+            styles.text,
+            {
+              marginTop: -5,
+              fontFamily: "Poppins_SemiBold",
+              color: "#42A45C",
+              marginBottom: 50,
+            },
+          ]}
+        >
+          VitaMind
+        </Text>
+        <View style={{ marginBottom: 10 }}>
+          <TextField
+            label="Full Name"
+            placeholder="Enter your full name"
+            value={user.fullname}
+            onChange={(text) => setUser({ ...user, fullname: text })}
+          />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <TextField
+            label="Email"
+            placeholder="Enter your email address"
+            value={user.email}
+            onChange={(text) => setUser({ ...user, email: text })}
+          />
+        </View>
+
+        <View style={{ marginBottom: 10 }}>
+          <TextField
+            label="Password"
+            placeholder="Enter your password"
+            value={user.password}
+            onChange={(text) => setUser({ ...user, password: text })}
+          />
+        </View>
+        <Animatable.View
+          style={{ marginBottom: 10 }}
+          animation={
+            user.confirmpassword.length > 0 &&
+            user.confirmpassword !== user.password
+              ? "shake"
+              : null
+          }
+        >
+          <TextField
+            label="Confirm Password"
+            placeholder="Confirm your password"
+            value={user.confirmpassword}
+            onChange={(text) => setUser({ ...user, confirmpassword: text })}
+            secureTextEntry
+            onChangeText={setConfirmPassword}
+          />
+        </Animatable.View>
+
+        <View
+          style={{
+            marginTop: 20,
+            marginBottom: 10,
+          }}
+        >
+          <Button
+            onPress={() => navigation.navigate("Home")}
+            title="Register"
+            disabled={(!user.email || !user.password) && true}
+          />
+        </View>
+
+        <View style={styles.register}>
+          <Text style={styles.text1}>Are you a member? </Text>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <Text
+              style={[
+                styles.text1,
+                { fontFamily: "Poppins_SemiBold", color: "#42A45C" },
+              ]}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+          <Text style={[styles.text1]}> here</Text>
+        </View>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
 
 export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "start",
+    paddingHorizontal: "7%",
+    paddingTop: 100,
+  },
+  text: {
+    fontSize: 42,
+    fontFamily: "Poppins",
+    textAlign: "left",
+  },
+  register: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  text1: {
+    fontFamily: "Lato",
+    fontSize: 14,
+    color: "#9E9C9B",
+  },
+  leftLine: {
+    height: 2,
+    width: "32%",
+    marginHorizontal: 5,
+  },
+});
