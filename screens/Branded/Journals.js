e5abe57f650d726d61d07942fc97b6ad67ca3dc5
LAPTOP-4C6P74KC\user <201810616@ua.edu.lb>
2023-05-07 19:06:27 +0300

    happy lynn

--- screens/Branded/Journals.js
+++ screens/Branded/Journals.js
@@ -1,17 +1,23 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import Button from '../../components/Button';
-import Header from '../../components/Header';
-import * as api from '../../controllers/ApiJournal';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import * as api from "../../controllers/ApiJournal";
 
 const Journals = ({ navigation }) => {
-  const [userId, setUserId] = useState('');
+  const [userId, setUserId] = useState("");
   const [journals, setJournals] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const userString = await AsyncStorage.getItem('user');
+      const userString = await AsyncStorage.getItem("user");
       setUserId(JSON.parse(userString)._id);
     }
     fetchData();
@@ -20,7 +26,7 @@ const Journals = ({ navigation }) => {
   useEffect(() => {
     try {
       api.getAllJournalsByUserId(userId).then((res) => {
-        console.log(res, 'LOL');
+        // console.log(res, 'LOL');
         setJournals(res);
       });
     } catch (error) {
@@ -34,31 +40,80 @@ const Journals = ({ navigation }) => {
 
       <ScrollView style={{ paddingHorizontal: 20 }}>
         {/* <TouchableOpacity>
-            <Text>Add</Text>
-        </TouchableOpacity> */}
-        <Button title="Create" onPress={() => navigation.navigate('Journal')} />
-        {journals.map((j, idx) => (
-          <View
-            key={idx}
-            style={{
-              width: '100%',
-              height: 100,
-              borderRadius: 25,
-              backgroundColor: 'white',
-              borderWidth: 2,
-              borderColor: 'green',
-              marginVertical: 15,
-              marginBottom: idx == journals.length - 1 && 200,
-              padding: 15,
-            }}
-          >
-            <Text>{j.text}</Text>
-            <Text>{j.date}</Text>
-          </View>
-        ))}
+          <Text>Add</Text>
+      </TouchableOpacity> */}
+        <Button title="Create" onPress={() => navigation.navigate("Journal")} />
+
+        {journals.map((j, idx) => {
+          const date = new Date(j.date);
+          const options = { weekday: "long", month: "long", day: "numeric" };
+          const formattedDate = date.toLocaleDateString(undefined, options);
+
+          if (
+            idx === 0 ||
+            formattedDate !==
+              new Date(journals[idx - 1].date).toLocaleDateString(
+                undefined,
+                options
+              )
+          ) {
+            return (
+              <View key={idx}>
+                <View style={{ marginTop: 20 }}>
+                  <Text style={styles.titleheading}>{formattedDate}</Text>
+                </View>
+                <View
+                  style={{
+                    width: "100%",
+                    height: 100,
+                    borderRadius: 25,
+                    backgroundColor: "white",
+                    borderWidth: 2,
+                    borderColor: "green",
+                    marginVertical: 15,
+                    marginBottom: idx === journals.length - 1 && 200,
+                    padding: 15,
+                  }}
+                >
+                  <Text>{j.text}</Text>
+                </View>
+              </View>
+            );
+          } else {
+            return (
+              <View
+                key={idx}
+                style={{
+                  width: "100%",
+                  height: 100,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  borderWidth: 2,
+                  borderColor: "green",
+                  marginVertical: 15,
+                  marginBottom: idx === journals.length - 1 && 200,
+                  padding: 15,
+                }}
+              >
+                <Text>{j.text}</Text>
+              </View>
+            );
+          }
+        })}
       </ScrollView>
     </View>
   );
 };
 
 export default Journals;
+
+const styles = StyleSheet.create({
+  titleheading: {
+    textAlign: "Left",
+    fontSize: 22,
+    fontFamily: "Poppins_SemiBold",
+    alignSelf: "left",
+    color: "#142F21",
+    paddingHorizontal: 15,
+  },
+});
